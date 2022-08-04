1d6311965a3d5e153eaef5b8bbfac985b4d9fdae
Roame <roamehildebrand@gmail.com>
2022-08-04 16:42:11 -0400

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -16,20 +16,24 @@
 int main() {
 	// Hello!
 
-//	GraphicsMgr& g = GraphicsMgr::getInstance();
-//	Camera* cam = g.getCamera();
-//	cam->pos.z = 1; // Cam is automatically pointing in the -z-direction btw
-//	cam->setProjection(Matrix::genOrtho(4, 3, 0, 100));
-//
-//	GraphicsMgr::WindowDims dims = g.getWDims();
-//
-//	while(g.active()){
-//		glClear(GL_COLOR_BUFFER_BIT);
-//
-//		g.swapBuffers();
-//		g.update();
-//	}
-//	g.stop();
+	GraphicsMgr& g = GraphicsMgr::getInstance();
+	Camera* cam = g.getCamera();
+	cam->pos.z = 1; // Cam is automatically pointing in the -z-direction btw
+	cam->setProjection(Matrix::genOrtho(4, 3, 0, 100));
+
+	GraphicsMgr::WindowDims dims = g.getWDims();
+//	Element elm = RectangleElm(Rectangle(0,0,0,1,1), Color(1,1,1,1));
+
+
+
+	while(g.active()){
+		glClear(GL_COLOR_BUFFER_BIT);
+//		g.draw
+
+		g.swapBuffers();
+		g.update();
+	}
+	g.stop();
 
 	return 0;
 }
